03f72208c46c5a7593639c6f341e3327e6e551db
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-09-30 16:20:11 -0300

    .

--- primeiros_codigos/problema1.c
+++ primeiros_codigos/problema1.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
+    int num;
+    printf("Digite um número qualquer: ");
+    scanf("%d",&num);    
+
+    int antecessor = num - 1;
+    int sucessor = num + 1;
+    
+    printf("o antecessor é %d\n",antecessor);
+    printf("o sucessor é %d\n",sucessor);
     return 0;
 }
