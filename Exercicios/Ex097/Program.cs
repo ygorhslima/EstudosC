f20b4c43d5afad9d348f3da2b043bf09ab5ba8f0
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-05-28 15:14:16 -0300

    terminando exercícios de console C#

---
+++ Exercicios/Ex097/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lido pelo teclado.
+No final, mostre a matriz na tela, com formatação correta.
+*/
+class Program
+{
+    static void Lin()
+    {
+        Console.WriteLine(string.Concat(Enumerable.Repeat("-=",20)));
+    }
+    public static void Main(string[] args)
+    {
+        int [,] matriz = new int[3,3];
+        Console.WriteLine("Digite os valores para preencher a matriz 3x3");
+        for(int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Digite um valor para [{i},{j}]: ");
+                matriz[i,j] = int.Parse(Console.ReadLine() ?? "");
+            }
+        }
+        Lin();
+        Console.WriteLine("MATRIZ RESULTANTE");
+        Lin();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($" [ {matriz[i,j]} ] ");
+            }
+            Console.WriteLine();
+        }
+        Lin();
+    }
+}
