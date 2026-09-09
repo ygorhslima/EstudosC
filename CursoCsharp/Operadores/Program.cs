39ad09a1d3d85befc41146e91f5e3ca0f4d06892
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-09-09 20:32:21 -0300

    mudando nome de pasta

---
+++ CursoCsharp/Operadores/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int num = 10;
+        // incremento
+        num++;
+        //num = num + 1;
+        //num += 1;
+        Console.WriteLine(num);
+
+        num--;
+        //num = num - 1;
+        //num -= 1;
+        //decremento
+        Console.WriteLine(num);
+
+        int num2 = 20;
+        int num3 = 50;
+
+
+        int soma = num2 + num3;
+        int subtracao = num2 - num3;
+        int multiplicacao = num2 * num3;
+        int divisao = num2 / num3;
+
+        Console.WriteLine($"{num2} + {num3} = {soma}");
+        Console.WriteLine($"{num2} - {num3} = {subtracao}");
+        Console.WriteLine($"{num2} * {num3} = {multiplicacao}");
+        Console.WriteLine($"{num2} / {num3} = {divisao}");
+
+        bool res = (90 > 5) && (10 > 20);
+        Console.WriteLine(res);
+    }    
+}
