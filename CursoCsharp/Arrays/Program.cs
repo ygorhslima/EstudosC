ec01de05c52b7b2f3c6ff06a70b1dbdb1eddf72e
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-09-09 21:23:11 -0300

    revisando arrays

---
+++ CursoCsharp/Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] names = ["Fredi","Mariana"];
+        /*
+        for(int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine($"{names[i]}");
+        }
+        */
+        foreach (var item in names)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
