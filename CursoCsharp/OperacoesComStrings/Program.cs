21419cf07967692eae32e0f11e0662bf2919ab40
Ygor Henrique <ygorlima.dev06@gmail.com>
2026-09-10 14:40:20 -0300

    Update Program.cs

--- CursoCsharp/OperacoesComStrings/Program.cs
+++ CursoCsharp/OperacoesComStrings/Program.cs
@@ -5,7 +5,7 @@
         string texto = "  Olá, Mundo do C#!   ";
         string textoVazio = "";
         string textoNulo = null;
-        
+
         // 1. Inspeção e Busca
         Console.WriteLine($"""
         === INSPEÇÃO E BUSCA ===
@@ -28,8 +28,34 @@
         Minúsculo: {limpo.ToLower()}
         Substituindo Mundo para Desenvolvedor: {limpo.Replace("Mundo", "Desenvolvedor")}
         Substring: {limpo.Substring(5, 5)}
-        PadLeft: {limpo.PadLeft(25, '*')}
-        PadRight: {limpo.PadRight(25, '*')}
+        Caracteres a mais à esquerda: {limpo.PadLeft(25, '*')}
+        Caracteres a mais à direita: {limpo.PadRight(25, '*')}
         """);
+
+
+        Console.WriteLine("\n=== DIVISÃO E JUNÇÃO ===");
+        string linguagensCsv = "C#,Java,Python,TypeScript";
+        string[] arrayLinguagens = linguagensCsv.Split(',');
+        Console.WriteLine($"Split (primeiro elemento): {arrayLinguagens[0]}");
+
+        string textoJuntado = string.Join(" | ", arrayLinguagens);
+        Console.WriteLine($"string.Join: {textoJuntado}");
+
+
+        // 4. Comparação
+        Console.WriteLine("\n=== COMPARAÇÃO ===");
+        string palavraA = "csharp";
+        string palavraB = "CSHARP";
+        Console.WriteLine($"Equals (ignorando case): {palavraA.Equals(palavraB, StringComparison.OrdinalIgnoreCase)}");
+        Console.WriteLine($"string.Compare: {string.Compare("Ana", "Bia")}");
+
+        // 5. Concatenação e Formatação
+        Console.WriteLine("\n=== CONCATENAÇÃO E FORMATAÇÃO ===");
+        string usuario = "Carlos";
+        int pontos = 150;
+
+        Console.WriteLine($"Interpolação ($): Usuário {usuario} possui {pontos} pontos.");
+        Console.WriteLine(string.Format("string.Format: Usuário {0} possui {1} pontos.", usuario, pontos));
+        Console.WriteLine(string.Concat("string.Concat: ", usuario, " ganho de pontos: ", pontos));
     }
 }
